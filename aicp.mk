b4a3e7d60af5b0e1250d51f71572cac8b607e804
Abhiram <abhiramshibu1998@gmail.com>
2016-12-23 06:53:12 +0530

    Moved and edited device makefile

--- aicp.mk
+++ aicp.mk
@@ -15,12 +15,11 @@
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
-
+$(call inherit-product, vendor/aicp/config/common_mini_phone.mk)
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := aicp_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
